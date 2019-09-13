84f7b17c7aa57067b24ef7c6bff665b447bfabfa
hubert_gorecki <hubert.gorecki@interia.pl>
2019-09-13 10:15:15 +0200

    Przeniesienie testów z unittest na py.test. Foxtury. Zadanie nr 4

---
+++ application_group.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+
+class ApplicationGroup:
+
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def wylogowanie(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def powrot_na_strone_z_lista_grup(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def utworzenie_nowej_grupy(self, Group):
+        wd = self.wd
+        self.otwiera_strone_z_grupami()
+        # Rozpoczynamy tworzenie nowej grupy
+        wd.find_element_by_name("new").click()
+        # Wypełniamy formularz danymi
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(Group.nazwa)
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(Group.naglowek)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(Group.stopka)
+        # Submit - Tworzymy nową grupę
+        wd.find_element_by_name("submit").click()
+        self.powrot_na_strone_z_lista_grup()
+
+    def otwiera_strone_z_grupami(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def zalogowanie(self, login, haslo):
+        wd = self.wd
+        self.otwarcie_strony_glownej()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(login)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(haslo)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def otwarcie_strony_glownej(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/group.php")
+
+    def destroy(self):
+        self.wd.quit()
