e899642de5f7d63bfe22456a7f0e686bf2fcf834
hubert_gorecki <hubert.gorecki@interia.pl>
2019-09-18 08:14:44 +0200

    Zadanie nr 7 - Wszystkie testy tworzenia, usuwania, modyfikacji dla grup i kontaktów

--- conftest.py
+++ conftest.py
@@ -6,7 +6,8 @@ from fixture.menager import Menager
 
 # scope = "session"  - jedno uruchomienie przegladarki i następnie uruchomienie testów.
 # Bez tego - każdy test = otwarcie przegladarki co wydłuża czas.
-@pytest.fixture(scope = "session")
+# @pytest.fixture(scope = "session") - nie działa
+@pytest.fixture
 # Fixtura dla wszystkich testów. Dzięki temu nie musimy umieszczać oddzielnej fixtury w każdym tescie./
 def app(request):
     fixture = Menager()
